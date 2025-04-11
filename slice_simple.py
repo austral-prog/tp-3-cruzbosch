0c07ac28433244cb3a64097f11c0281c40638cd7
cruzbosch <cpbosch@mail.austral.edu.ar>
2025-04-11 11:57:32 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,17 +1,8 @@
-texto = "Awesome".lower()
+def slice_simple():
+    texto = "Awesome"
+    print(texto[:3].lower())
+    print(texto[2:5])
+    print(texto.lower())
 
-# Primeras 3 letras
-primeras_tres = texto[0:3]
-
-# Las 3 letras en el medio
-mitad = len(texto) // 2
-tres_del_medio = texto[mitad - 1:mitad + 2]
-
-# De la primera a la cuarta (incluida) y de la antepenúltima a la última (incluida)
-primera_a_cuarta = texto[0:5]
-antepenultima_a_ultima = texto[-3:]
-
-# Imprimir resultados
-print(primeras_tres)
-print(tres_del_medio)
-print(primera_a_cuarta + antepenultima_a_ultima)
+slice_simple()
+  
