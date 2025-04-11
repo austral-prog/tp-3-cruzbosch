ffcda44de1df2837938d5e20bd4b098eec6d7129
cruzbosch <cpbosch@mail.austral.edu.ar>
2025-04-11 11:58:56 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,9 +1,6 @@
-def slice_advanced():
-    # Código a implementar utilizando input.
-    texto = input("Ingresa texto:")
+texto = input("Ingresá un texto: ")
 
-    print(texto[4: :2])
+# Obtener caracteres desde el quinto (índice 4) en adelante, saltando de a 2
+resultado = texto[4::2]
 
-slice_advanced()
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+print(resultado)
